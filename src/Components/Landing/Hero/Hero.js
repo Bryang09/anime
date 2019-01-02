a92af80d7cabaa81d98429bded21a32654a73aaf
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-01-02 15:50:48 -0600

    Project Started

---
+++ src/Components/Landing/Hero/Hero.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+import "./Hero.scss";
+
+class Hero extends Component {
+  state = {
+    fullScreen: true
+  };
+  render() {
+    return (
+      <div
+        className="Hero"
+        style={
+          this.state.fullScreen === false
+            ? { height: "80vh" }
+            : { height: "100vh" }
+        }
+      >
+        <h1>Welcome To Bryan's Anime Listings</h1>
+        <h3
+          onClick={() => this.setState({ fullScreen: !this.state.fullScreen })}
+        >
+          Discover Anime Now!
+        </h3>
+      </div>
+    );
+  }
+}
+
+export default Hero;
