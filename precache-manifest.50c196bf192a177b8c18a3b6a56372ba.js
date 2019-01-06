97b5f09e7efaf8e0fc98202292e6f13e44358bf4
bryan.gonzalez96.bg@gmail.com <bryan.gonzalez96.bg@gmail.com>
2019-01-06 14:22:07 -0600

    Updates

---
+++ precache-manifest.50c196bf192a177b8c18a3b6a56372ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "03dfa2d1ba63c35c34b3",
+    "url": "/anime/static/js/runtime~main.03dfa2d1.js"
+  },
+  {
+    "revision": "2fa5772c5a4ec6f882d2",
+    "url": "/anime/static/js/main.2fa5772c.chunk.js"
+  },
+  {
+    "revision": "03373f22d4b667d701b8",
+    "url": "/anime/static/js/1.03373f22.chunk.js"
+  },
+  {
+    "revision": "2fa5772c5a4ec6f882d2",
+    "url": "/anime/static/css/main.a93c718c.chunk.css"
+  },
+  {
+    "revision": "5a99192beeef3afea0faded398b96ef4",
+    "url": "/anime/index.html"
+  }
+];
